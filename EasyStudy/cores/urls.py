de29e10569caab9d205e5f96cecfa35c76035d13
Mirafzal <mirafzal@172-15-251-27.lightspeed.irvnca.sbcglobal.net>
2024-05-22 12:38:09 +0500

    second commit rename main app

--- EasyStudy/cores/urls.py
+++ EasyStudy/cores/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for EasyStudy project.
+URL configuration for cores project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- EasyStudy/cores/wsgi.py
+++ EasyStudy/cores/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for EasyStudy project.
+WSGI config for cores project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EasyStudy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cores.settings')
 
 application = get_wsgi_application()
